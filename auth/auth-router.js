53506ef721cc2669660309ba18d03869befffb85
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-24 09:55:09 -0600

    Implement registering a new user

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -10,6 +10,19 @@ router.post("/register", (req, res) => {
       .json({ errorMessage: "username and password are required" });
     return;
   }
+
+  db("users")
+    .insert(creds, "id")
+    .then((ids) => {
+      return db("users").select("id", "username").where({ id: ids[0] }).first();
+    })
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: err.message });
+    });
 });
 
 router.post("/login", (req, res) => {
--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,8 +1,13 @@
 const request = require("supertest");
 const app = require("../api/server.js");
+const db = require("../database/dbConfig.js");
 
 describe("auth-router", () => {
   describe("POST /api/auth/register", () => {
+    beforeEach(() => {
+      db("users").truncate();
+    });
+
     it("rejects invalid credential parameters", async () => {
       const response = await request(app)
         .post("/api/auth/register")
@@ -10,7 +15,15 @@ describe("auth-router", () => {
       expect(response.status).toBe(400);
     });
 
-    it.todo("returns the new user id");
+    it("returns the new user", async () => {
+      const response = await request(app)
+        .post("/api/auth/register")
+        .send({ username: "mithrandir", password: "melloc" });
+
+      expect(response.status).toBe(201);
+      expect(response.body.id).toBeTruthy();
+      expect(response.body.username).toBe("mithrandir");
+    });
   });
 
   describe("POST /api/auth/login", () => {
