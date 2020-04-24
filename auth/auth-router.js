d678783a7b3bf79ce4f3f7eddeade0489181999c
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-04-24 09:42:02 -0600

    Check for invalid register requests

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,18 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+const db = require("../database/dbConfig.js");
+
+router.post("/register", (req, res) => {
+  const creds = req.body;
+  if (creds.username === undefined || creds.password === undefined) {
+    res
+      .status(400)
+      .json({ errorMessage: "username and password are required" });
+    return;
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,12 +1,19 @@
 const request = require("supertest");
+const app = require("../api/server.js");
 
 describe("auth-router", () => {
-  describe("POST /api/register", () => {
-    it.todo("rejects invalid credential parameters");
+  describe("POST /api/auth/register", () => {
+    it("rejects invalid credential parameters", async () => {
+      const response = await request(app)
+        .post("/api/auth/register")
+        .send({ username: "passwordless_joe" });
+      expect(response.status).toBe(400);
+    });
+
     it.todo("returns the new user id");
   });
 
-  describe("POST /api/login", () => {
+  describe("POST /api/auth/login", () => {
     it.todo("rejects invalid credentials");
     it.todo("returns a token for valid users");
   });
